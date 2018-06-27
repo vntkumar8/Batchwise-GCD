cff020b6ebb3e4ed7818833f5c0541cea303f6e0
Vineet Kumar <vntkumar8@gmail.com>
2018-06-27 19:33:51 +0530

    Scripts added

---
+++ union.sh
@@ -0,0 +1,147 @@
+
+echo " taken 2 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.3/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.4/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.2/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 3 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.4/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 4 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.4/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 5 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 6 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.7/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 7 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.7/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.6/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 8 at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.1/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.4/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.8/vm_aggregated 32.7/vm_aggregated 32.1/vm_aggregated > uni
+sort -u uni | wc -l
+cat 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated 32.1/vm_aggregated > uni
+sort -u uni | wc -l
+
+echo " taken 9 (ALL) at time"
+cat 32.1/vm_aggregated 32.2/vm_aggregated 32.3/vm_aggregated 32.4/vm_aggregated 32.5/vm_aggregated 32.6/vm_aggregated 32.7/vm_aggregated 32.8/vm_aggregated 32.9/vm_aggregated > uni
+sort -u uni | wc -l
+
